49dfd6a9d8a8b17452f9073e179c9b40c7ad071d
Miles Herron <94080621+milesherron@users.noreply.github.com>
2023-03-28 17:22:31 +0000

    added dynamic data and api route

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Account Manager',
+    'location': 'United States',
+    'salary': 'Commission'
+  },
+]
+
+
+@app.route("/")
+def hello_world():
+  return render_template('home.html', jobs=JOBS, company_name='Miles Agency')
+
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
